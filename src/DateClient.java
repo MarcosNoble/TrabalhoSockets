e9d8c4a9cbcca52dbb26b4278d034c57fe042550
Marcos <marcos.luz.noble@gmail.com>
2023-04-09 16:36:13 -0300

    Inicio T1, copiando codigos do exercicio

--- src/DateClient.java
+++ src/DateClient.java
@@ -6,7 +6,7 @@ import java.io.IOException;
  * A command line client for the date server. Requires the IP address of the
  * server as the sole argument. Exits after printing the response.
  */
-public class DateClient {
+public class DateClient{
     public static void main(String[] args) throws IOException {
         if (args.length != 1) {
             System.err.println("Pass the server IP as the sole command line argument");
